ed7d7fb447ad14abd53ffd477a9f1ccfa5b231cb
Gerardo R Padilla Jr <gerardopadillareynoso@gmail.com>
2021-02-07 20:49:40 -0600

    Add files via upload

--- Dictionary.java
+++ Dictionary.java
@@ -1,26 +1,35 @@
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Dictionary {
+	static Map<String, Word> m = new HashMap<String, Word>();
 
 	public static void main(String[] args) {
-		Map<String, Word> m = new HashMap<String, Word>();
 		BufferedReader dictionaryReader = null;
 		File fileName = new File("Dictionary.txt");
 		int currIndex = -1;
 		Word newWord;
-
-
+		DictionaryGUI display = new DictionaryGUI();
 		String first4 = "";
 		String word = "";
 		String currentLine = "";
 		Word.Gtype currType = null;
-
 		try {
 
 			dictionaryReader = new BufferedReader(new FileReader(fileName));
@@ -60,7 +69,7 @@ public class Dictionary {
 						newWord = new Word(word, currentLine, currType);
 						// System.out.println(newWord);
 						m.put(word, newWord);
-						
+
 						word = "";
 						currentLine = "";
 						currType = null;
@@ -76,23 +85,101 @@ public class Dictionary {
 
 			try {
 				if (dictionaryReader != null)
-					dictionaryReader.close();
+					System.out.println("Dictionary Has been initialized with all the words");
+				dictionaryReader.close();
 			} catch (IOException ex) {
 				ex.printStackTrace();
 			}
+
 		}
 
 		// System.out.println(m.size() + " distinct words:");
 		// System.out.println(m);
-		Scanner keyboard = new Scanner(System.in);
-		String wordToSearch = "Integer";
-		System.out.println("Enter a Word to Search For.");
-		wordToSearch = keyboard.nextLine();
-		wordToSearch=wordToSearch.trim();
-		wordToSearch = wordToSearch.substring(0, 1).toUpperCase() + wordToSearch.substring(1);
-		
 
-		System.out.println(m.get(wordToSearch) );
-		keyboard.close();
+	}
+
+	public static Word getDicWord(String word) {
+		Word thisWord = m.get(word);
+		return thisWord;
+	}
+
+	public static boolean isWord(String word) {
+		return m.containsKey(word);
+	}
+
+}
+
+class DictionaryGUI implements ActionListener {
+
+	JFrame frame = new JFrame("The Best Java Dictionary");
+	JLabel label = new JLabel("Enter a Word to get its definition: 	");
+	JLabel definition = new JLabel("");
+	JButton defineButton = new JButton("Define");
+	JTextField textBox = new JTextField(20);
+	Dictionary myDic = new Dictionary();
+	// Creating the panel at bottom and adding components
+	JPanel panel = new JPanel();
+	JPanel panel2 = new JPanel();
+
+	public DictionaryGUI() {
+
+		defineButton.addActionListener(this);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		panel.setBorder(BorderFactory.createEmptyBorder(100, 100, 100, 100));
+		panel.setLayout(new GridLayout(3, 2));
+		panel2.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
+//		panel2.setLayout(new GridLayout(0, 1));
+		panel.add(label);
+		panel.add(textBox);
+		panel.add(defineButton);
+
+		panel2.add(definition);
+		frame.add(panel, BorderLayout.NORTH);
+		frame.add(panel2, BorderLayout.CENTER);
+		frame.pack();
+		frame.setSize(1000, 400);
+		frame.setVisible(true);
+
+	}
+
+	public String getWord() {
+
+		String word = "";
+		if (textBox.getText() != null) {
+
+			word = textBox.getText();
+		}
+		return word;
+
+	}
+
+	/**
+	 * @Override
+	 * 
+	 *           This method will execute when the "Define" button is pressed
+	 * 
+	 * 
+	 */
+	public void actionPerformed(ActionEvent e) {
+
+		if (!textBox.getText().isEmpty()) {
+			String word = textBox.getText();
+
+			word = word.trim();
+			word = word.substring(0, 1).toUpperCase() + word.substring(1);
+			if (Dictionary.isWord(word))
+				show(Dictionary.getDicWord(word));
+			else
+				definition.setText("Word Not in Dictionary.\n Try Again");
+
+		} else {
+			definition.setText("Please, enter a Word before pressing Button!");
+		}
+
+	}
+
+	public void show(Word word) {
+		definition.setText(word.toString());
 	}
 }
--- Word.java
+++ Word.java
@@ -4,21 +4,22 @@
  *         This code is a basic
  * 
  */
+
 public class Word {
 
 	private String word;
 	private String definition;
 	private Gtype type;
 
-	enum Gtype {
+	public enum Gtype {
 		noun, verb, adjective
 	};
 
 	/**
 	 * 
-	 * @param new Word    word to add
-	 * @param Def 	definition of that word
-	 * @param newType	type of the new word
+	 * @param new     Word word to add
+	 * @param Def     definition of that word
+	 * @param newType type of the new word
 	 */
 	Word(String newWord, String Def, Gtype newType) {
 		setWord(newWord);
@@ -33,18 +34,39 @@ public class Word {
 		setType(NewWord.getType());
 	}
 
+	/**
+	 * Returns the word that is here
+	 * 
+	 * @return word that is stored in this class
+	 */
 	public String getWord() {
 		return word;
 	}
 
+	/**
+	 * Sets the word member to the parameter
+	 * 
+	 * @param word to be defined
+	 *
+	 */
 	public void setWord(String word) {
 		this.word = word;
 	}
 
+	/**
+	 * Returns the definition of the word
+	 * 
+	 * @return definition of the word
+	 */
 	public String getDefinition() {
 		return definition;
 	}
 
+	/**
+	 * Sets the definition member to the parameter
+	 * 
+	 * @param definition to be added
+	 */
 	public void setDefinition(String definition) {
 		this.definition = definition;
 	}
@@ -54,7 +76,20 @@ public class Word {
 	}
 
 	public void setType(Gtype gType) {
+
 		this.type = gType;
+	}
+
+	/**
+	 * Will print the word, type of word, and a definition
+	 * 
+	 * @return the entire word and its information on a single line
+	 */
+	@Override
+	public String toString() {
+		
+		return (word + ": (" + type + ") " + definition);
+
 	}
 
 }
